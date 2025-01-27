37a42fb1968674bb077c9adbeb99fabd5185f71e
jmetsaniemi1 <166294290+jmetsaniemi1@users.noreply.github.com>
2025-01-28 00:02:29 +0200

    1.1.2.

--- JAVASCRIPT/darkmode.js
+++ JAVASCRIPT/darkmode.js
@@ -1,24 +1,28 @@
 let darkmode = localStorage.getItem('darkmode');
 const themeSwitch = document.getElementById('theme-switch');
 
-// Check if dark mode is already enabled on page load
+// Tarkista, onko dark mode päällä sivun latauksen yhteydessä
 if (darkmode === "active") {
-    document.body.classList.add('darkmode');
+  document.body.classList.add('darkmode');
+  document.body.classList.remove('default');
 } else {
-    document.body.classList.remove('darkmode');
+  document.body.classList.add('default');
+  document.body.classList.remove('darkmode');
 }
 
-// Toggle dark mode on button click
+// Vaihda tila napin painalluksella
 themeSwitch.addEventListener('click', () => {
-    darkmode = localStorage.getItem('darkmode');
-    
-    if (darkmode !== "active") {
-        document.body.classList.add('darkmode');
-        localStorage.setItem('darkmode', 'active');
-    } else {
-        document.body.classList.remove('darkmode');
-        localStorage.setItem('darkmode', null);
-    }
+  darkmode = localStorage.getItem('darkmode');
+  
+  if (darkmode !== "active") {
+    document.body.classList.add('darkmode');
+    document.body.classList.remove('default');
+    localStorage.setItem('darkmode', 'active');
+  } else {
+    document.body.classList.add('default');
+    document.body.classList.remove('darkmode');
+    localStorage.setItem('darkmode', null);
+  }
 });
 
 
