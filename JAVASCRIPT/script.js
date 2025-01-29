f7428a49107fa87fa5f9b098a9376c7e472c3436
jmetsaniemi1 <166294290+jmetsaniemi1@users.noreply.github.com>
2025-01-29 20:54:30 +0200

    Heading fixed

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -1,3 +1,45 @@
+// DARKMODE
+
+document.addEventListener('DOMContentLoaded', () => {
+  const themeSwitch = document.getElementById('theme-switch');
+  const body = document.body;
+  const moonSvg = document.getElementById('moon-svg');
+  const sunSvg = document.getElementById('sun-svg');
+
+  // Tarkista tallennettu teema
+  const savedTheme = localStorage.getItem('theme');
+  if (savedTheme === 'dark') {
+    body.classList.add('darkmode');
+    moonSvg.style.display = 'none';
+    sunSvg.style.display = 'block';
+  } else {
+    moonSvg.style.display = 'block';
+    sunSvg.style.display = 'none';
+  }
+
+  themeSwitch.addEventListener('click', () => {
+    if (body.classList.contains('darkmode')) {
+      // Vaihto light modeen viiveellä
+      setTimeout(() => {
+        body.classList.remove('darkmode');
+        moonSvg.style.display = 'block';
+        sunSvg.style.display = 'none';
+        localStorage.setItem('theme', 'light');
+      }, 300);
+    } else {
+      // Vaihto dark modeen viiveellä
+      setTimeout(() => {
+        body.classList.add('darkmode');
+        moonSvg.style.display = 'none';
+        sunSvg.style.display = 'block';
+        localStorage.setItem('theme', 'dark');
+      }, 300);
+    }
+  });
+});
+
+// HAMMENU
+
 const hamMenu = document.querySelector(".ham-menu");
 
 const offScreenMenu = document.querySelector(".off-screen-menu");
