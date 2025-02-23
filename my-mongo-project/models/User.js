1a6b30a59d0082db04b42455d371015bbf8308eb
jmetsaniemi1 <166294290+jmetsaniemi1@users.noreply.github.com>
2025-02-23 17:37:36 +0200

    debugging

--- my-mongo-project/models/User.js
+++ my-mongo-project/models/User.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     passwordHash: { type: String, required: true },
-    role: { type: String, default: "user" },
+    role: { type: String, default: "user", enum: ["user", "admin"] },
     is_active: { type: Boolean, default: true }
-}, { collection: 'Users' }); // Pakotetaan käyttämään 'Users' kokoelmaa
+}, { collection: 'Users' });
 
 const User = mongoose.model('User', UserSchema);
 
 module.exports = User;
+
